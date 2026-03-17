c0f1ec94e2403317f4c829c5a2116437b42224b8
CJ3034496 <CJ3034496@LAB01-PC35.ifspcjoad.ifspcjo.edu.br>
2026-03-17 19:40:20 -0300

    Aula 03/03/2006

---
+++ Project2/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int numero1, numero2 = 3, soma;
+		numero1 = 4;
+		soma = numero1 + numero2;
+		System.out.println("Soma = " + soma);
+	}
+
+}
